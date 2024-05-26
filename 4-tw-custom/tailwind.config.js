0f2572dd114f054bd7cec0147642aefed2c33878
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-05-26 13:58:01 +0630

    font-ok

--- 4-tw-custom/tailwind.config.js
+++ 4-tw-custom/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+       'poppins' : [ "Poppins"],
+       'roboto' : ["Roboto"],
+       'roboto-padauk' : ["Roboto","Padauk"],
+       'padauk' : ["Padauk"],
+       'noto-mm' : ["Noto Sans Myanmar"],
+      },
+    },
   },
   plugins: [],
 }
