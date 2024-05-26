18d55e636979000a0f4c7e8008c6bf7d4176544e
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-05-26 14:18:49 +0630

    colors

--- 4-tw-custom/tailwind.config.js
+++ 4-tw-custom/tailwind.config.js
@@ -9,9 +9,28 @@ module.exports = {
        'roboto-padauk' : ["Roboto","Padauk"],
        'padauk' : ["Padauk"],
        'noto-mm' : ["Noto Sans Myanmar"],
+       'tharlon' : ["TharLon"],
+       'thu' : ["MM-Thu"],
+       'mm-angoun' : ["MyanmarAngoun"],
+      },
+      colors : {
+        black: {
+          100: "#ccd3e4",
+          200: "#99a7c9",
+          300: "#667baf",
+          400: "#334f94",
+          500: "#002379",
+          600: "#001c61",
+          700: "#001549",
+          800: "#000e30",
+          900: "#000718"
+      },
+     "orange-second" : "#FF5F00",
       },
     },
   },
   plugins: [],
 }
+
+
 
