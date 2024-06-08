295819154daa87879af3be14b73fdbf51fbe6f34
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-06-08 15:41:34 +0700

    js basic start

---
+++ 5-javascript/basic/basic.js
@@ -0,0 +1,31 @@
+let x = 5;
+let y = 10;
+let myName = "hein htet zan";
+let myAge = 18;
+let width = 18;
+let breadth = 60;
+let birthYear = 1994;
+let z = x + y;
+
+// console.log(`The total of x + y equal to ${x + y}`);
+// // console.log("The total of x + y equal to " + (x + y));
+// console.log(`Area of my room is ${width * breadth} square feet`);
+// console.log(`My name is ${myName},I'm ${2024 - birthYear} years old`);
+
+// console.log(x);
+// console.log(y);
+// console.log(myName);
+// console.log(myAge);
+// console.log(x);
+// console.log(y);
+
+let age = 26;
+let weight = 120.65;
+let girlFriend = 0;
+let temp = -10;
+
+console.log(typeof age);
+console.log(age);
+console.log(weight);
+console.log(girlFriend);
+console.log(temp);
