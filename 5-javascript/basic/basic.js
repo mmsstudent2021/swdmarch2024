25514915c78df3cd9f56db3ab74872cf1c8faad2
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-06-15 15:30:08 +0700

    finish js conditional and loop

--- 5-javascript/basic/basic.js
+++ 5-javascript/basic/basic.js
@@ -128,35 +128,149 @@
 // find age ? if you know birth year
 // age = currentYear - birthYear
 
-function findAge(birthYear) {
-  let currentYear = 2024;
-  //   const birthYear = 1994;
-  let age = currentYear - birthYear;
-  //   console.log(age);
-  return age;
+// function findAge(birthYear) {
+//   let currentYear = 2024;
+//   //   const birthYear = 1994;
+//   let age = currentYear - birthYear;
+//   //   console.log(age);
+//   return age;
+// }
+
+// // my age 1994
+// console.log(findAge(1994));
+
+// // mg mg 2003
+// console.log(findAge(2003));
+
+// // su su 1999
+// console.log(findAge(1999));
+
+// // zaw zaw 2010
+// console.log(findAge(2010));
+
+// // area function (area = width * breadth)
+
+// function area(width, breadth) {
+//   const calcArea = width * breadth;
+//   return calcArea + " sqft";
+// }
+
+// // find the area of my room. my room width is 18 and breadth 60
+// console.log(area(18, 60));
+
+// // find the area of my sister home. my room width is 12 and breadth 50
+// console.log(area(12, 50));
+
+// let x = 5;
+// let y = "5";
+
+// // normal mode
+// console.log(x == y);
+
+// // strict mode
+// console.log(x === y);
+
+// console.log(x !== y);
+
+// console.log(y == x ? "it is true" : "it is false");
+
+// function triangle(char = 0) {
+//   console.log(`${char}`);
+//   console.log(`${char} ${char}`);
+//   console.log(`${char} ${char} ${char}`);
+//   return "finish";
+//   console.log(`${char} ${char} ${char} ${char}`);
+//   console.log(`${char} ${char} ${char} ${char} ${char}`);
+// }
+
+// // call - invoke
+
+// console.log(triangle("*"));
+// triangle("x");
+// triangle("a");
+// triangle();
+
+// (amount/100)*percentage
+
+// function calculatePercentage(amount, percentage) {
+//   return (amount / 100) * percentage;
+// }
+
+// // console.log(calculatePercentage(1000, 5));
+// // console.log(calculatePercentage(4200000, 5));
+
+// function calculateTotal(amount = 0, taxRate = 5) {
+//   return amount + (amount / 100) * taxRate;
+// }
+
+// console.log("ice cream - ", calculateTotal(1000));
+// console.log("water - ", calculateTotal(200));
+// console.log("nothing - ", calculateTotal());
+// console.log("KIA - ", calculateTotal(120000000, 50));
+// console.log("Neta V - ", calculateTotal(70000000, 0));
+
+// function sum(x = 0, y = 0) {
+//   console.log(x + y);
+// }
+
+// sum(5, 4);
+// sum();
+
+// let x;
+// console.log(x);
+
+// console.log("*");
+// console.log("* *");
+// console.log("* * *");
+
+// console.log("*");
+// console.log("* *");
+// console.log("* * *");
+
+// if (false) {
+//   console.log("*");
+//   console.log("* *");
+//   console.log("* * *");
+// } else {
+//   console.log("* * *");
+//   console.log("* * *");
+//   console.log("* * *");
+// }
+
+// const passedMark = 40;
+// const yourMark = 15;
+
+// if (yourMark >= passedMark) {
+//   console.log("U pass");
+// } else {
+//   console.log("U fail");
+// }
+
+// low stock
+
+// const stock = 15;
+
+// if (stock >= 10) {
+//   console.log("in stock");
+// } else if (stock > 0) {
+//   console.log("low stock");
+// } else {
+//   console.log("out of stock");
+// }
+
+// const yourMark = 30;
+
+// if (yourMark >= 80) {
+//   console.log("D");
+// } else if (yourMark >= 40) {
+//   console.log("P");
+// } else {
+//   console.log("F");
+// }
+
+for (let i = 1; i <= 15; i += 3) {
+  // console.log("*");
+  // console.log("* *");
+  // console.log("* * *");
+  console.log("loop ", i);
 }
-
-// my age 1994
-console.log(findAge(1994));
-
-// mg mg 2003
-console.log(findAge(2003));
-
-// su su 1999
-console.log(findAge(1999));
-
-// zaw zaw 2010
-console.log(findAge(2010));
-
-// area function (area = width * breadth)
-
-function area(width, breadth) {
-  const calcArea = width * breadth;
-  return calcArea + " sqft";
-}
-
-// find the area of my room. my room width is 18 and breadth 60
-console.log(area(18, 60));
-
-// find the area of my sister home. my room width is 12 and breadth 50
-console.log(area(12, 50));
