500a88fe6d87b9f440e788df0837ce0b97f47315
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-06-16 15:26:06 +0700

    object finish

--- 5-javascript/basic/basic.js
+++ 5-javascript/basic/basic.js
@@ -268,9 +268,196 @@
 //   console.log("F");
 // }
 
-for (let i = 1; i <= 15; i += 3) {
-  // console.log("*");
-  // console.log("* *");
-  // console.log("* * *");
-  console.log("loop ", i);
-}
+// for (let i = 1; i <= 15; i += 3) {
+//   // console.log("*");
+//   // console.log("* *");
+//   // console.log("* * *");
+//   console.log("loop ", i);
+// }
+
+// function result(mark) {
+//   // if (mark >= 40) {
+//   //   return "Pass";
+//   // }
+//   if(mark >= 80){
+//     return "D";
+//   }else if(mark >= 40){
+//     return "P";
+//   }
+//   return "Fail";
+// }
+
+// console.log(result(35));
+// console.log(result(65));
+// console.log(result(85));
+
+// function blockMaker(width, height) {
+//   // return `* * *\n* * *\n* * *`;
+
+//   let result = ``;
+//   for(let x=1;x<=height;x++){
+//     for (let i = 1; i <= width; i++) {
+//       result += "* ";
+//     }
+//     result += "\n"
+//   }
+//   return result;
+// }
+
+// console.log(blockMaker(5, 4));
+// console.log(blockMaker(3, 3));
+// console.log(blockMaker(7, 5));
+
+// let x = null;
+// console.log(x);
+
+// let y;
+// console.log(y);
+
+// let x = 5;
+// x = 7;
+
+// const students = ["Mg Mg", "Kyaw Kyaw", "Zaw Zaw"];
+// const fruits = ["orange", "mango", "banana", "lemon", "lime"];
+
+// console.log(students);
+// console.log(fruits);
+
+// console.log(students[1]);
+// console.log(fruits[3]);
+
+// const chars = [];
+// chars[0] = "a";
+// chars[1] = "b";
+// chars[2] = "c";
+// chars[7] = "d";
+// console.log(chars);
+// console.log(chars[5]);
+
+// console.log(students.length);
+// console.log(fruits.length);
+
+// const mySelf = {
+//   myName: "hein htet zan",
+//   myAge: 18,
+//   myJob: "Developer",
+//   weight : 120.65,
+//   skill : ["html","css","js"],
+//   girlFriend : null,
+//   handsome : true
+// };
+// const nrcNo = {
+//   sateOrDivisionNo: 12,
+//   township: "BaHaNa",
+//   type: "N",
+//   no: 123123,
+// };
+
+// console.log(mySelf);
+// console.log(nrcNo);
+
+// console.log(mySelf["myJob"]);
+// console.log(mySelf.myJob);
+
+// console.log(nrcNo.no);
+
+// const info = {};
+
+// info["name"] = "Mg Mg";
+// info["gender"] = "Male";
+// info.job = "Student";
+// info.money = 5000;
+
+// console.log(info);
+
+// console.log(typeof chars);
+// console.log(typeof info);
+
+// const charArray = [
+//   ["a", "b", "c"],
+//   ["d", "e", "f"],
+//   ["g", "h", "i"],
+// ];
+
+// console.log(charArray);
+// console.log(charArray[1]);
+// console.log(charArray[1][1]);
+
+// console.log(charArray[2]);
+// console.log(charArray[2][2]);
+
+// const threeDArray = [
+//   [
+//     ["a", "b", "c"],
+//     ["d", "e", "f"],
+//   ],
+//   [
+//     ["g", "h", "i"],
+//     ["j", "k", "l"],
+//   ],
+// ];
+
+// console.log(threeDArray);
+// console.log(threeDArray[1]);
+// console.log(threeDArray[1][0]);
+// console.log(threeDArray[1][0][2]);
+
+const SWD = [
+  {
+    name: "mg mg",
+    gender: "male",
+    age: 18,
+  },
+  {
+    name: "su su",
+    gender: "female",
+    age: 17,
+  },
+  {
+    name: "hla hla",
+    gender: "female",
+    age: 18,
+  },
+  {
+    name: "zaw zaw",
+    gender: "male",
+    age: 17,
+  },
+];
+
+const products = [
+  {
+    name: "Laptop",
+    price: 999.99,
+    stock: 10,
+  },
+  {
+    name: "Smartphone",
+    price: 699.99,
+    stock: 25,
+  },
+  {
+    name: "Tablet",
+    price: 499.99,
+    stock: 15,
+  },
+  {
+    name: "Smartwatch",
+    price: 199.99,
+    stock: 30,
+  },
+  {
+    name: "Headphones",
+    price: 149.99,
+    stock: 50,
+  },
+];
+
+console.log(SWD);
+console.log(SWD[2]);
+console.log(SWD[2]["name"]);
+console.log(SWD[2].name);
+
+console.log(products);
+console.log(products[3]);
+console.log(products[3].stock);
