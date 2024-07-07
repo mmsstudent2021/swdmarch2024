bcc24d09289da11dba68e3ce3e0904cd1bf26080
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-07-07 14:07:03 +0700

    oop day 2 start

--- 5-javascript/basic/js/pp.js
+++ 5-javascript/basic/js/pp.js
@@ -104,66 +104,118 @@ const st5 = {
 // }
 // console.log(run);
 
-class Student {
-  static x = 5;
-  static run() {
-    return "I'm run";
+// class Student {
+//   static x = 5;
+//   static run() {
+//     return "I'm run";
+//   }
+
+//   // private property
+//   #subjects = {
+//     swd: ["html", "css", "js"],
+//     wad: ["php", "oop", "mysql", "laravel"],
+//   };
+
+//   constructor(inputId, inputName, inputGender, inputMajor) {
+//     // data initialize
+//     this.id = inputId;
+//     this.name = inputName;
+//     this.gender = inputGender;
+//     this.major = inputMajor;
+//   }
+
+//   showSubject() {
+//     return this.#subjects[this.major];
+//   }
+
+//   learn() {
+//     return this.name + " can learn";
+//   }
+// }
+
+// console.log(Student.run());
+// console.log(Student.x);
+
+// const me = new Student(136, "hein htet zan", "male", "swd");
+// console.log(me);
+// // console.log(me.learn());
+// console.log(me.major);
+// console.log(me.name);
+// console.log(me.id);
+// console.log(me.learn());
+// // console.log(me.#subjects);
+// console.log(me.showSubject());
+
+// const st1 = new Student(165, "Mg Mg", "male", "wad");
+// console.log(st1);
+// // console.log(st1.learn());
+// console.log(st1.major);
+
+// const st2 = new Student(147, "Su Su", "female", "swd");
+// // console.log(st2);
+
+// const st3 = new Student(46, "Aye Aye", "female", "swd");
+// // console.log(st3);
+
+// const st4 = new Student(15, "Zaw Zaw", "male", "wad");
+// // console.log(st4);
+
+// console.dir(Date);
+
+// console.log(Date.now());
+
+// const now = new Date();
+// console.dir(now);
+// console.log(now.toDateString());
+
+class Person {
+  // name,age
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
   }
 
-  // private property
-  #subjects = {
-    swd: ["html", "css", "js"],
-    wad: ["php", "oop", "mysql", "laravel"],
-  };
-
-  constructor(inputId, inputName, inputGender, inputMajor) {
-    // data initialize
-    this.id = inputId;
-    this.name = inputName;
-    this.gender = inputGender;
-    this.major = inputMajor;
-  }
-
-  showSubject() {
-    return this.#subjects[this.major];
-  }
-
-  learn() {
-    return this.name + " can learn";
+  talk() {
+    return this.name + " can talk";
   }
 }
 
-console.log(Student.run());
-console.log(Student.x);
+class Student extends Person {
+  constructor(name, age, gender, major) {
+    super(name, age, gender);
+    this.major = major;
+  }
+  learn() {
+    return `${this.name} can learn`;
+  }
+}
 
-const me = new Student(136, "hein htet zan", "male", "swd");
+class Monitor extends Student {
+  constructor(name, age, gender, major, roomNo) {
+    super(name, age, gender, major);
+    this.roomNo = roomNo;
+  }
+  command() {
+    return `${this.name} can command student from room ${this.roomNo}`;
+  }
+}
+
+const me = new Person("hein htet zan", 18, "male");
 console.log(me);
-// console.log(me.learn());
-console.log(me.major);
-console.log(me.name);
-console.log(me.id);
-console.log(me.learn());
-// console.log(me.#subjects);
-console.log(me.showSubject());
+console.log(me.talk());
 
-const st1 = new Student(165, "Mg Mg", "male", "wad");
+const st1 = new Student("Mg Mg", 16, "male", "swd");
 console.log(st1);
-// console.log(st1.learn());
-console.log(st1.major);
+console.log(st1.talk());
+console.log(st1.learn());
 
-const st2 = new Student(147, "Su Su", "female", "swd");
-// console.log(st2);
+const st2 = new Student("Su Su", 17, "female", "wad");
+console.log(st2);
 
-const st3 = new Student(46, "Aye Aye", "female", "swd");
-// console.log(st3);
-
-const st4 = new Student(15, "Zaw Zaw", "male", "wad");
-// console.log(st4);
-
-console.dir(Date);
-
-console.log(Date.now());
-
-const now = new Date();
-console.dir(now);
-console.log(now.toDateString());
+const m1 = new Monitor("Kyaw Kyaw", 17, "male", "swd", 5);
+console.log(m1);
+console.log(m1.name);
+console.log(m1.learn());
+console.log(m1.talk());
+console.log(m1.command());
